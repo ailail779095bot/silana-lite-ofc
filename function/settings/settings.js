76b6a87fc7d7c612c4878469540c5260d70da46a
ailail779095bot <ailail779095@gmail.com>
2026-02-26 01:24:11 +0300

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -1,4 +1,4 @@
-import { watchFile, unwatchFile } from 'fs'
+99import { watchFile, unwatchFile } from 'fs'
 import fs from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
@@ -15,7 +15,7 @@ global.setting = {
 
 global.info = {
  nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ pairingNumber : '967777735555',
  figlet: 'silanalite', // create a start console display
  nomorwa : '212605784394',
  nameown : 'NOUREDDINE',
