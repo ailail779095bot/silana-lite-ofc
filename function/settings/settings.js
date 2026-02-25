4d62cdc2a741f100fefec220dfb908f08de47f8a
ailail779095bot <ailail779095@gmail.com>
2026-02-26 00:31:15 +0300

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,12 +14,12 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '967777735555',
+ pairingNumber : '967777735555',
  figlet: 'silanalite', // create a start console display
- nomorwa : '212605784394',
+ nomorwa : '967777735555',
  nameown : 'NOUREDDINE',
- nomerown : '212717457920',
+ nomerown : '967777735555',
  packname : 'sticker by ',
  author : 'SILANA AI',
  namebot : '乂 SILANA AI',
